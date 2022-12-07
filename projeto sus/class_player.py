1c17a8d3bed1c83d94eedcebaa7d0e36a74f0fe8
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2022-12-06 22:46:13 -0300

    sus - classes

---
+++ projeto sus/class_player.py
@@ -0,0 +1,40 @@
+import pygame as pg
+from os import getcwd
+
+
+class Player:
+    def __init__(self):
+        # files
+        self.diretorio = getcwd()
+        # images
+        self.imposter_sprite = pg.image.load(f'{self.diretorio}\\images\\ciano.png')
+        # pos
+        self.x_imposter = 250
+        self.y_imposter = 250
+        # others
+        self.flip = False
+
+    def flip_sprite(self):
+        pg.transform.flip(self.imposter_sprite, True, False)
+
+    def move(self, event):
+        if event.key == pg.K_w:
+            self.y_imposter -= 45
+        if event.key == pg.K_s:
+            self.y_imposter += 45
+
+        if event.key == pg.K_a:
+            self.x_imposter -= 45
+            if self.flip:
+                pass
+            else:
+                # pg.transform.flip(self.imposter_sprite, True, False)
+                self.flip = True
+
+        if event.key == pg.K_d:
+            self.x_imposter += 45
+            if self.flip:
+                # pg.transform.flip(self.imposter_sprite, True, False)
+                self.flip = False
+            else:
+                pass
--- projeto sus/main.py
+++ projeto sus/main.py
@@ -1,78 +1,59 @@
+from class_player import Player
+
 import pygame as pg
 from random import randint
+from os import getcwd
+from sys import exit
+
 pg.init()
 pg.mixer.init()
+
+diretorio = getcwd()  # remover dps
+# classe
+player = Player()
 # images
-ciano_sprite = pg.image.load('C:\\Users\\887382\\Downloads\\projeto sus\\images\\ciano.png')
-roxo_sprite = pg.image.load('C:\\Users\\887382\\Downloads\\projeto sus\\images\\roxo.png')
-roxo_body_sprite = pg.image.load('C:\\Users\\887382\\Downloads\\projeto sus\\images\\body_roxo.png')
-icon = pg.image.load('C:\\Users\\887382\\Downloads\\projeto sus\\images\\icone.png')
+roxo_sprite = pg.image.load(f'{diretorio}\\images\\roxo.png')
+roxo_body_sprite = pg.image.load(f'{diretorio}\\images\\body_roxo.png')
+icon = pg.image.load(f'{diretorio}\\images\\icone.png')
 
-# window config
 resolution = [1280, 720]
-x_ciano = 250
-y_ciano = 250
-x_roxo = randint(10, resolution[0]-60)
-y_roxo = randint(10, resolution[1]-60)
-virou = False
-
 window = pg.display.set_mode((resolution[0], resolution[1]))
+fundo = pg.Color(128, 128, 128)
+font = pg.font.SysFont('arial.ttf', 50)
+x_roxo = randint(10, resolution[0] - 60)
+y_roxo = randint(10, resolution[1] - 60)
 pg.display.set_caption('SUS')
 pg.display.set_icon(icon)
-
-fundo = pg.Color(128, 128, 128)
-# sounds
-death_sound = pg.mixer.Sound('C:\\Users\\887382\\Downloads\\projeto sus\\sounds\\death.mp3')
-
-# textos
-font = pg.font.SysFont('arial.ttf', 50)
-
-# vars
+death_sound = pg.mixer.Sound(f'{diretorio}\\sounds\\death.wav')
 points = 0
 life = True
 
 while True:
     window.fill(fundo)
+
     text_points = font.render(f'Points: {points}', True, (255, 255, 255), fundo)
     window.blit(text_points, (1000, 50))
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
+            exit()
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_w:
-                y_ciano -= 45
-            if event.key == pg.K_s:
-                y_ciano += 45
+            player.move(event=event)
 
-            if event.key == pg.K_a:
-                x_ciano -= 45
-                if virou:
-                    pass
-                else:
-                    ciano_sprite = pg.transform.flip(ciano_sprite, True, False)
-                    virou = True
-
-            if event.key == pg.K_d:
-                x_ciano += 45
-                if virou:
-                    ciano_sprite = pg.transform.flip(ciano_sprite, True, False)
-                    virou = False
-                else:
-                    pass
-
-    ciano = window.blit(ciano_sprite, (x_ciano, y_ciano))
+    cian = window.blit(player.imposter_sprite, (player.x_imposter, player.y_imposter))
     roxo = window.blit(roxo_sprite, (x_roxo, y_roxo))
 
-    if ciano.colliderect(roxo):
-            x_roxo_body = x_roxo
-            y_roxo_body = y_roxo
-            window.blit(roxo_body_sprite, (x_roxo_body, y_roxo_body))
-            x_roxo = randint(10, resolution[0] - 60)
-            y_roxo = randint(10, resolution[1] - 60)
+    if cian.colliderect(roxo):
+        x_roxo_body = x_roxo
+        y_roxo_body = y_roxo
+        window.blit(roxo_body_sprite, (x_roxo_body, y_roxo_body))
+        x_roxo = randint(10, resolution[0] - 60)
+        y_roxo = randint(10, resolution[1] - 60)
 
-            pg.mixer.Sound.play(death_sound)
+        pg.mixer.Sound.play(death_sound)
 
-            points += 1
-            life = False
+        points += 1
+        life = False
 
     pg.display.flip()
