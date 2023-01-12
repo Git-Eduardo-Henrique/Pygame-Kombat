ea86f03fd4fcea53a5b5b96c8b68035f1314325c
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-01-11 23:38:15 -0300

    mk jump

--- projeto mk/teste.py
+++ projeto mk/teste.py
@@ -15,7 +15,7 @@ background_image = pygame.transform.scale(background_image,
 # Carrega as imagens dos personagens
 player_image = pygame.image.load("player.png")
 player_x = 50
-player_y = screen_height - player_image.get_height()
+player_y = (screen_height - player_image.get_height())
 
 enemy_image = pygame.image.load("enemy.gif")
 enemy_x = screen_width - enemy_image.get_width() - 50
@@ -29,7 +29,14 @@ damage_radius = 50
 # Define a fonte e o tamanho do texto
 font = pygame.font.Font(None, 36)
 
-# Define o loop do jogo
+# definir velocidade de queda do personagem
+fall_speed = 0
+speed = 3
+
+# definir força do pulo
+jump_strength = -20
+in_air = False
+
 running = True
 while running:
     screen.blit(background_image, (-(screen_width / 2), 0))
@@ -44,7 +51,7 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             # Verifica se a tecla clicada é a tecla espaço
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_g:
                 distance = ((player_x - enemy_x) ** 2 + (player_y - enemy_y) ** 2) ** 0.5
                 if distance < damage_radius:
                     # Causa dano no inimigo
@@ -52,17 +59,49 @@ while running:
                         enemy_health -= 10
                     else:
                         pass
+            elif event.key == pygame.K_w:
+                if not in_air:
+                    fall_speed = jump_strength
+                    in_air = True
+                else:
+                    in_air = False
 
+        # checar estado do controle de jogo
+    joystick_count = pygame.joystick.get_count()
+    if joystick_count > 0:
+        # usando o primeiro controle conectado
+        joystick = pygame.joystick.Joystick(0)
+        joystick.init()
+
+        # obter valores dos eixos do controle
+        x_axis = joystick.get_axis(0)
+        y_axis = joystick.get_axis(1)
+    elif joystick.get_button(0):
+        if not in_air:
+            fall_speed = jump_strength
+            in_air = True
+        else:
+            in_air = False
+
+            # mover personagem de acordo com os eixos
+    player_x += x_axis * speed
+    player_y += y_axis * speed
+    # detectar tecla de pulo pressionada
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        player_x -= 0.5
-    if keys[pygame.K_RIGHT]:
-        player_x += 0.5
 
-    # Atualiza a posição do inimigo
-    enemy_x += 0.1
-    if enemy_x > screen_width - enemy_image.get_width():
-        enemy_x = screen_width - enemy_image.get_width()
+    if keys[pygame.K_a]:
+        player_x -= speed
+    if keys[pygame.K_d]:
+        player_x += speed
+    # aplicar velocidade de queda a posição do personagem
+    player_y += fall_speed
+
+    if player_y >= (screen_height - player_image.get_height()):
+        fall_speed = 0
+        player_y = (screen_height - player_image.get_height())
+    else:
+        # aplicar gravidade
+        fall_speed += 1
 
     # Desenha os personagens na tela
     screen.blit(text, (screen_width - 100, 10))
@@ -72,4 +111,3 @@ while running:
     pygame.display.flip()
 
 pygame.quit()
-
