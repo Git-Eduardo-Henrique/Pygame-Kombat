dfdeee05a661b92f94681993c18ba13e3e1941ab
Eduardo Henrique <96957574+Git-Eduardo-Henrique@users.noreply.github.com>
2023-01-21 16:41:23 -0300

    mk scorpion walking

--- projeto mk/main.py
+++ projeto mk/main.py
@@ -8,18 +8,17 @@ pygame.init()
 diretorio = getcwd()
 
 # Definir as dimensões da tela
-screen_width = 750
-screen_height = 350
+screen_width = 1000
+screen_height = 400
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 background_image = pygame.image.load(f"{diretorio}\\images\\stage.png")
 background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
 
-
 # Definir as vidas iniciais dos personagens
-player1_life = 100
-player2_life = 100
-max_life = 100
+max_life = 1000
+player1_life = max_life
+player2_life = max_life
 
 # Definir a fonte para mostrar as vidas
 life_font = pygame.font.Font(None, 30)
@@ -31,9 +30,11 @@ running = True
 
 # Definir a cor da barra de vida
 health_color = (0, 255, 0)
+demage_color = (255, 0, 0)
 
 # Definir a posição e dimensões da barra de vida
-health_rect = pygame.Rect(10, 25, 350, 20)
+health_rect = pygame.Rect(10, 25, 440, 20)
+health_rect_2 = pygame.Rect(550, 25, 440, 20)
 
 scorpion = scorpion(screen_height=screen_height)
 sub_zero = sub_zero(screen_height=screen_height, screen_width=screen_width)
@@ -49,28 +50,39 @@ while running:
 
     sub_zero.update(screen=screen)
     sub_zero.move()
-    scorpion.update(screen=screen)
-    scorpion.move()
+
+    scorpion.events(
+        screen=screen,
+        animation_normal=scorpion.scorpion_normal,
+        animation_normal_reverse=scorpion.scorpion_normal_inv,
+        animation_walking=scorpion.scorpion_walking,
+        animation_walking_reverse=scorpion.scorpion_walking_inv
+    )
 
     # Desenhar as vidas dos personagens na tela
-    player2_life_text = life_font.render("Player 2: " + str(player2_life), True, (255, 255, 255))
-    screen.blit(player2_life_text, (screen_width - 150, 50))
-    pygame.draw.rect(screen, health_color, (health_rect.x, health_rect.y, player1_life/max_life * health_rect.width, health_rect.height))
+    player2_life_text = life_font.render(f"Player 1: {str(player1_life)} Player 2: {str(player2_life)}", True, (255, 255, 255))
+    screen.blit(player2_life_text, (10, 50))
+    pygame.draw.rect(screen, demage_color, (health_rect.x, health_rect.y, health_rect.width, health_rect.height))
+    pygame.draw.rect(screen, health_color,
+                     (health_rect.x, health_rect.y, player1_life / max_life * health_rect.width, health_rect.height))
+
+    pygame.draw.rect(screen, demage_color, (health_rect_2.x, health_rect_2.y, health_rect_2.width, health_rect_2.height))
+    pygame.draw.rect(screen, health_color,
+                     (health_rect_2.x, health_rect_2.y, player2_life / max_life * health_rect_2.width, health_rect_2.height))
 
     if pressed_keys[pygame.K_l]:
-        player1_life -= 1
+        player1_life -= 10
     elif pressed_keys[pygame.K_p]:
         player2_life -= 10
 
-    # Verificar se algum personagem morreu
+    # Verificar se algum personagem more
     if player1_life <= 0 or player2_life <= 0:
         game_over_text = final_font.render("finish him", True, (255, 255, 255))
-        screen.blit(game_over_text, (screen_width/2 - game_over_text.get_width()/2, screen_height/2 - game_over_text.get_height()/2))
+        screen.blit(game_over_text, (
+            screen_width / 2 - game_over_text.get_width() / 2, screen_height / 2 - game_over_text.get_height() / 2))
 
     # Atualizar a tela
     pygame.display.flip()
 
 # Finalizar o pygame
 pygame.quit()
-
-
--- projeto mk/scorpion.py
+++ projeto mk/scorpion.py
@@ -5,35 +5,65 @@ from os import getcwd
 class scorpion:
     def __init__(self, screen_height):
         self.diretorio = getcwd()
-        self.scorpion_animation = []
-        self.scorpion_animation_inv = []
-        for i in range(1, 8):
-            image = pygame.image.load(f"{self.diretorio}\\images\\scorpion\\scorpion_{i}.png")
-            self.scorpion_animation.append(image)
-            self.scorpion_animation_inv.append(pygame.transform.flip(image, True, False))
+        self.scorpion_normal = []
+        self.scorpion_normal_inv = []
+
+        self.scorpion_walking = []
+        self.scorpion_walking_inv = []
+        for i in range(1, 7):
+            image = pygame.image.load(f"{self.diretorio}\\images\\scorpion\\scorpion_normal\\scorpion_normal_{i}.png")
+            image_upscale = pygame.transform.scale(image, (image.get_width() * 2, image.get_height() * 2))
+            self.scorpion_normal.append(image_upscale)
+            self.scorpion_normal_inv.append(pygame.transform.flip(image_upscale, True, False))
+
+        for i in range(1, 9):
+            image = pygame.image.load(f"{self.diretorio}\\images\\scorpion\\scorpion_walk\\scorpion_walking_{i}.png")
+            image_upscale = pygame.transform.scale(image, (image.get_width() * 2, image.get_height() * 2))
+            self.scorpion_walking.append(image_upscale)
+            self.scorpion_walking_inv.append(pygame.transform.flip(image_upscale, True, False))
 
         self.scorpion_state = True
         self.animation_index = 0
         self.scorpion_x = 50
-        self.scorpion_y = screen_height - self.scorpion_animation[0].get_height() - 25
+        self.scorpion_y = screen_height - self.scorpion_normal[0].get_height() - 25
+        self.walking_state = False
 
-    def update(self, screen):
+    def update(self, screen, animation, animation_reverse):
         if self.scorpion_state:
             self.animation_index += 0.15
-            if self.animation_index >= len(self.scorpion_animation):
+            if self.animation_index >= len(animation):
                 self.animation_index = 0
-            screen.blit(self.scorpion_animation[int(self.animation_index)], (self.scorpion_x, self.scorpion_y))
+            screen.blit(animation[int(self.animation_index)], (self.scorpion_x, self.scorpion_y))
         else:
             self.animation_index += 0.15
-            if self.animation_index >= len(self.scorpion_animation_inv):
+            if self.animation_index >= len(animation_reverse):
                 self.animation_index = 0
-            screen.blit(self.scorpion_animation_inv[int(self.animation_index)], (self.scorpion_x, self.scorpion_y))
+            screen.blit(animation_reverse[int(self.animation_index)], (self.scorpion_x, self.scorpion_y))
 
-    def move(self):
+    def events(self, screen, animation_normal, animation_normal_reverse, animation_walking, animation_walking_reverse):
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[pygame.K_a]:
             self.scorpion_x -= 5
             self.scorpion_state = False
-        if pressed_keys[pygame.K_d]:
+            self.update(
+                screen=screen,
+                animation=animation_walking,
+                animation_reverse=animation_walking_reverse
+            )
+
+        elif pressed_keys[pygame.K_d]:
             self.scorpion_x += 5
             self.scorpion_state = True
+            self.update(
+                screen=screen,
+                animation=animation_walking,
+                animation_reverse=animation_walking_reverse
+            )
+        else:
+            self.update(
+                screen=screen,
+                animation=animation_normal,
+                animation_reverse=animation_normal_reverse
+            )
+
+
